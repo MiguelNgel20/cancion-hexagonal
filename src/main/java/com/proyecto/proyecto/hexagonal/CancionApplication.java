844ed7dd8af8a2f4b78f1fc0c62f17ec5ff0cbc5
MiguelNgel20 <mricardom1@unicartagena.edu.co>
2025-10-28 13:45:35 -0500

    Estructura basada en hexagonal

---
+++ src/main/java/com/proyecto/proyecto/hexagonal/CancionApplication.java
@@ -0,0 +1,12 @@
+package com.proyecto.proyecto.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CancionApplication.class, args);
+    }
+}
